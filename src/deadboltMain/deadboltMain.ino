ad4219e3cfe6b71334ed6d1d2e744d9376bd4687
jake <jake@jake-pc.jkindley.net>
2026-01-11 09:41:11 -0800

    feat: fix base functionality

--- src/deadboltMain/deadboltMain.ino
+++ src/deadboltMain/deadboltMain.ino
@@ -1,14 +1,16 @@
 #include <SPI.h>
 #include <MFRC522.h>
-#include <Servo.h> 
+#include <ESP32Servo.h>
 
-#define SS_PIN 10
-#define RST_PIN 9
-#define SERVO_PIN 8
+#define SS_PIN 21
+#define RST_PIN 22
+#define SERVO_PIN 25
+
+#define APPROVED_USER_COUNT 5
  
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
-byte allowedUIDs[5][4];
+byte approvedUIDs[APPROVED_USER_COUNT][4] = $APPROVED_UIDS
 byte lastUid[4];
 Servo servo; 
 
@@ -19,16 +21,19 @@ void setup() {
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
 
-  servo.attach(SERVO_PIN)
+  servo.attach(SERVO_PIN);
 
   setLock(false);
+  Serial.println("Started");
 }
 
 void loop() {
-  sleep(1000)
+  delay(500);
+  Serial.println("loop start");
 
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
   if ( ! rfid.PICC_IsNewCardPresent()) {
+    setLastUid(new byte[4]{0,0,0,0});
     return;
   }
 
@@ -36,6 +41,7 @@ void loop() {
   if ( ! rfid.PICC_ReadCardSerial()) {
     return;
   }
+  rfid.PICC_HaltA();
 
   printUid();
 
@@ -43,12 +49,18 @@ void loop() {
     if (isApprovedUid(rfid.uid.uidByte)) {
       setLock(!isLocked); // toggle the lock when activated
     } else {
-      Serial.write("AAHH UNAPPROVED UID!!!")
+      Serial.println("AAHH UNAPPROVED UID!!!");
     }
   } else {
-    Serial.write("Card already read!");
+    Serial.println("Card already read!");
   }
+  setLastUid(rfid.uid.uidByte);
+}
 
+void setLastUid(byte uid[]) {
+  for(int i = 0; i < 4; i ++) {
+    lastUid[i] = uid[i];
+  }
 }
 
 bool isNewCard(byte uid[]) {
@@ -56,7 +68,7 @@ bool isNewCard(byte uid[]) {
 }
 
 bool isApprovedUid(byte uid[]) {
-  for (int i=0; i< approvedUIDs.length; i++) {
+  for (int i=0; i < APPROVED_USER_COUNT; i++) {
     if (areUidsMatching(uid, approvedUIDs[i])) {
       return true;
     }
@@ -65,10 +77,10 @@ bool isApprovedUid(byte uid[]) {
 }
 
 bool areUidsMatching(byte uid1[], byte uid2[]) {
-  if (uid1.length != 4 || uid2.length != 4) {
-    Serial.write("UID unexpected length!");
-    return false;
-  }
+  // if (uid1.length != 4 || uid2.length != 4) {
+  //   Serial.println("UID unexpected length!");
+  //   return false;
+  // }
   return uid1[0] == uid2[0] &&
     uid1[1] == uid2[1] &&
     uid1[2] == uid2[2] &&
